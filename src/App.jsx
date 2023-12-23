5bb04c885e5b270869bdef5180f94acf73fb4985
Mahardhikas <129693294+Mahardhikas@users.noreply.github.com>
2023-12-23 18:31:30 +0700

    new head

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
-import Header from './components/header'
 import './App.css'
-import Banner from './components/Banner'
+import Banner from './components/Banner.jsx'
 import logo from './assets/logo.png'
 import bannerImage from './assets/banner-image.jpg'
 import ListPost from './components/ListPost'
 import Card from './components/Card'
 import Nomor from './components/Nomor'
+import Header from './components/header'
 
 function App() {
   const [activeMenu, setActiveMenu] = useState('Ideas');
@@ -66,37 +66,8 @@ function App() {
 
   return (
     <div className='window'>
-        <header>
-          <div>
-            <nav className='fixed-header'>
-              <div>
-                <img src={logo} alt=" "/>
-              </div>
-              <div>
-                <ul className='nav_list'>
-                  <li className={`nav_item ${activeMenu === 'Work' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Work')}>Work</li>
-                  <li className={`nav_item ${activeMenu === 'About' ? 'active-link' : ''}`} onClick={() => handleMenuClick('About')}>About</li>
-                  <li className={`nav_item ${activeMenu === 'Services' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Services')}>Services</li>
-                  <li className={`nav_item ${activeMenu === 'Ideas' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Ideas')}>Ideas</li>
-                  <li className={`nav_item ${activeMenu === 'Careers' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Careers')}>Careers</li>
-                  <li className={`nav_item nav_ujung ${activeMenu === 'Contact' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Contact')}>Contact</li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-          </header>
-
-          <main>
-          <section className={`banner ${isScrolling ? 'is-scrolling' : ''}`}>
-            <div className="parallax-container">
-              <img src={bannerImage} alt="Banner" className="parallax-image" />
-              <div className="banner-text">
-                <h1>Ideas</h1>
-                <p>Where All Our Great Things Begin</p>
-              </div>
-            </div>
-          </section>
-          </main>
+        <Header/>
+        <Banner/>
           <div classname="mx-20">
             <ListPost/>
             <div className='grid md:grid-cols-3 xl:grid-cols-4 grid-cols-1 gap-2 xl:gap-4 my-8 xl:mx-20 mx-0 md:mx-8'>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,22 +12,22 @@ const Header = () => {
   
     return (
       <div>
-        <nav className='fixed-header'>
-          <div>
-            <img src={logo} alt=" "/>
+            <nav className='fixed-header'>
+              <div>
+                <img src={logo} alt=" "/>
+              </div>
+              <div>
+                <ul className='nav_list'>
+                  <li className={`nav_item ${activeMenu === 'Work' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Work')}>Work</li>
+                  <li className={`nav_item ${activeMenu === 'About' ? 'active-link' : ''}`} onClick={() => handleMenuClick('About')}>About</li>
+                  <li className={`nav_item ${activeMenu === 'Services' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Services')}>Services</li>
+                  <li className={`nav_item ${activeMenu === 'Ideas' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Ideas')}>Ideas</li>
+                  <li className={`nav_item ${activeMenu === 'Careers' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Careers')}>Careers</li>
+                  <li className={`nav_item nav_ujung ${activeMenu === 'Contact' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Contact')}>Contact</li>
+                </ul>
+              </div>
+            </nav>
           </div>
-          <div>
-            <ul className='nav_list'>
-              <li className={`nav_item ${activeMenu === 'Work' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Work')}>Work</li>
-              <li className={`nav_item ${activeMenu === 'About' ? 'active-link' : ''}`} onClick={() => handleMenuClick('About')}>About</li>
-              <li className={`nav_item ${activeMenu === 'Services' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Services')}>Services</li>
-              <li className={`nav_item ${activeMenu === 'Ideas' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Ideas')}>Ideas</li>
-              <li className={`nav_item ${activeMenu === 'Careers' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Careers')}>Careers</li>
-              <li className={`nav_item nav_ujung ${activeMenu === 'Contact' ? 'active-link' : ''}`} onClick={() => handleMenuClick('Contact')}>Contact</li>
-            </ul>
-          </div>
-        </nav>
-      </div>
     );
   }
   
